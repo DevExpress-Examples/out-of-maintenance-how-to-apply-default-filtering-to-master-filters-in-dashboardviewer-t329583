815da6645672b36a848f999c78d6d82ac68c2dbf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_MFDefaultValues/Dashboard1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Dashboard_MFDefaultValues {
+    partial class Dashboard1 {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            DevExpress.DashboardCommon.Dimension dimension1 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.GridDimensionColumn gridDimensionColumn1 = new DevExpress.DashboardCommon.GridDimensionColumn();
+            DevExpress.DashboardCommon.Measure measure1 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.GridMeasureColumn gridMeasureColumn1 = new DevExpress.DashboardCommon.GridMeasureColumn();
+            DevExpress.DataAccess.Sql.TableQuery tableQuery1 = new DevExpress.DataAccess.Sql.TableQuery();
+            DevExpress.DataAccess.Sql.TableInfo tableInfo1 = new DevExpress.DataAccess.Sql.TableInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo1 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo2 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo3 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo4 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo5 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo6 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo7 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo8 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo9 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo10 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo11 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            DevExpress.DataAccess.Sql.ColumnInfo columnInfo12 = new DevExpress.DataAccess.Sql.ColumnInfo();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard1));
+            DevExpress.DashboardCommon.Dimension dimension2 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.Measure measure2 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.Dimension dimension3 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.ChartPane chartPane1 = new DevExpress.DashboardCommon.ChartPane();
+            DevExpress.DashboardCommon.SimpleSeries simpleSeries1 = new DevExpress.DashboardCommon.SimpleSeries();
+            DevExpress.DashboardCommon.Dimension dimension4 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.Measure measure3 = new DevExpress.DashboardCommon.Measure();
+            DevExpress.DashboardCommon.SimpleSeries simpleSeries2 = new DevExpress.DashboardCommon.SimpleSeries();
+            DevExpress.DashboardCommon.Dimension dimension5 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.Dimension dimension6 = new DevExpress.DashboardCommon.Dimension();
+            DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup1 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
+            DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup2 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
+            DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup3 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem1 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem2 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem3 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem4 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            this.gridDashboardItem1 = new DevExpress.DashboardCommon.GridDashboardItem();
+            this.dashboardSqlDataSource1 = new DevExpress.DashboardCommon.DashboardSqlDataSource();
+            this.chartDashboardItem1 = new DevExpress.DashboardCommon.ChartDashboardItem();
+            this.rangeFilterDashboardItem1 = new DevExpress.DashboardCommon.RangeFilterDashboardItem();
+            this.treeViewDashboardItem1 = new DevExpress.DashboardCommon.TreeViewDashboardItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardSqlDataSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rangeFilterDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(measure3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treeViewDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // gridDashboardItem1
+            // 
+            dimension1.DataMember = "Sales Person";
+            gridDimensionColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridDimensionColumn1.AddDataItem("Dimension", dimension1);
+            measure1.DataMember = "Extended Price";
+            measure1.Name = "Extended Price";
+            gridMeasureColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight;
+            gridMeasureColumn1.AddDataItem("Measure", measure1);
+            this.gridDashboardItem1.Columns.AddRange(new DevExpress.DashboardCommon.GridColumnBase[] {
+            gridDimensionColumn1,
+            gridMeasureColumn1});
+            this.gridDashboardItem1.ComponentName = "gridDashboardItem1";
+            this.gridDashboardItem1.DataItemRepository.Clear();
+            this.gridDashboardItem1.DataItemRepository.Add(measure1, "DataItem1");
+            this.gridDashboardItem1.DataItemRepository.Add(dimension1, "DataItem0");
+            this.gridDashboardItem1.DataMember = "SalesPerson";
+            this.gridDashboardItem1.DataSource = this.dashboardSqlDataSource1;
+            this.gridDashboardItem1.GridOptions.ColumnWidthMode = DevExpress.DashboardCommon.GridColumnWidthMode.Manual;
+            this.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = true;
+            this.gridDashboardItem1.InteractivityOptions.MasterFilterMode = DevExpress.DashboardCommon.DashboardItemMasterFilterMode.Single;
+            this.gridDashboardItem1.Name = "Grid 1";
+            this.gridDashboardItem1.ShowCaption = false;
+            // 
+            // dashboardSqlDataSource1
+            // 
+            this.dashboardSqlDataSource1.ComponentName = "dashboardSqlDataSource1";
+            this.dashboardSqlDataSource1.ConnectionName = "nwind";
+            this.dashboardSqlDataSource1.Name = "SQL Data Source 1";
+            tableQuery1.Name = "SalesPerson";
+            tableInfo1.Name = "SalesPerson";
+            columnInfo1.Name = "OrderID";
+            columnInfo2.Name = "Country";
+            columnInfo3.Name = "FirstName";
+            columnInfo4.Name = "LastName";
+            columnInfo5.Name = "ProductName";
+            columnInfo6.Name = "CategoryName";
+            columnInfo7.Name = "OrderDate";
+            columnInfo8.Name = "UnitPrice";
+            columnInfo9.Name = "Quantity";
+            columnInfo10.Name = "Discount";
+            columnInfo11.Name = "Extended Price";
+            columnInfo12.Name = "Sales Person";
+            tableInfo1.SelectedColumns.AddRange(new DevExpress.DataAccess.Sql.ColumnInfo[] {
+            columnInfo1,
+            columnInfo2,
+            columnInfo3,
+            columnInfo4,
+            columnInfo5,
+            columnInfo6,
+            columnInfo7,
+            columnInfo8,
+            columnInfo9,
+            columnInfo10,
+            columnInfo11,
+            columnInfo12});
+            tableQuery1.Tables.AddRange(new DevExpress.DataAccess.Sql.TableInfo[] {
+            tableInfo1});
+            this.dashboardSqlDataSource1.Queries.AddRange(new DevExpress.DataAccess.Sql.SqlQuery[] {
+            tableQuery1});
+            this.dashboardSqlDataSource1.ResultSchemaSerializable = resources.GetString("dashboardSqlDataSource1.ResultSchemaSerializable");
+            // 
+            // chartDashboardItem1
+            // 
+            dimension2.DataMember = "CategoryName";
+            this.chartDashboardItem1.Arguments.AddRange(new DevExpress.DashboardCommon.Dimension[] {
+            dimension2});
+            this.chartDashboardItem1.AxisX.TitleVisible = false;
+            this.chartDashboardItem1.ComponentName = "chartDashboardItem1";
+            measure2.DataMember = "Extended Price";
+            measure2.Name = "Extended Price";
+            dimension3.DataMember = "OrderDate";
+            this.chartDashboardItem1.DataItemRepository.Clear();
+            this.chartDashboardItem1.DataItemRepository.Add(measure2, "DataItem1");
+            this.chartDashboardItem1.DataItemRepository.Add(dimension2, "DataItem2");
+            this.chartDashboardItem1.DataItemRepository.Add(dimension3, "DataItem3");
+            this.chartDashboardItem1.DataMember = "SalesPerson";
+            this.chartDashboardItem1.DataSource = this.dashboardSqlDataSource1;
+            this.chartDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
+            this.chartDashboardItem1.Name = "Sales by Catgory";
+            chartPane1.Name = "Pane 1";
+            chartPane1.PrimaryAxisY.AlwaysShowZeroLevel = true;
+            chartPane1.PrimaryAxisY.ShowGridLines = true;
+            chartPane1.PrimaryAxisY.TitleVisible = true;
+            chartPane1.SecondaryAxisY.AlwaysShowZeroLevel = true;
+            chartPane1.SecondaryAxisY.ShowGridLines = false;
+            chartPane1.SecondaryAxisY.TitleVisible = true;
+            simpleSeries1.AddDataItem("Value", measure2);
+            chartPane1.Series.AddRange(new DevExpress.DashboardCommon.ChartSeries[] {
+            simpleSeries1});
+            this.chartDashboardItem1.Panes.AddRange(new DevExpress.DashboardCommon.ChartPane[] {
+            chartPane1});
+            this.chartDashboardItem1.SeriesDimensions.AddRange(new DevExpress.DashboardCommon.Dimension[] {
+            dimension3});
+            this.chartDashboardItem1.ShowCaption = true;
+            // 
+            // rangeFilterDashboardItem1
+            // 
+            dimension4.DataMember = "OrderDate";
+            dimension4.DateTimeGroupInterval = DevExpress.DashboardCommon.DateTimeGroupInterval.MonthYear;
+            this.rangeFilterDashboardItem1.Argument = dimension4;
+            this.rangeFilterDashboardItem1.ComponentName = "rangeFilterDashboardItem1";
+            measure3.DataMember = "Extended Price";
+            measure3.Name = "Extended Price";
+            this.rangeFilterDashboardItem1.DataItemRepository.Clear();
+            this.rangeFilterDashboardItem1.DataItemRepository.Add(dimension4, "DataItem0");
+            this.rangeFilterDashboardItem1.DataItemRepository.Add(measure3, "DataItem1");
+            this.rangeFilterDashboardItem1.DataMember = "SalesPerson";
+            this.rangeFilterDashboardItem1.DataSource = this.dashboardSqlDataSource1;
+            this.rangeFilterDashboardItem1.InteractivityOptions.IgnoreMasterFilters = true;
+            this.rangeFilterDashboardItem1.Name = "Range Filter 1";
+            simpleSeries2.SeriesType = DevExpress.DashboardCommon.SimpleSeriesType.Area;
+            simpleSeries2.AddDataItem("Value", measure3);
+            this.rangeFilterDashboardItem1.Series.AddRange(new DevExpress.DashboardCommon.SimpleSeries[] {
+            simpleSeries2});
+            this.rangeFilterDashboardItem1.ShowCaption = false;
+            // 
+            // treeViewDashboardItem1
+            // 
+            this.treeViewDashboardItem1.ComponentName = "treeViewDashboardItem1";
+            dimension5.DataMember = "CategoryName";
+            dimension5.Name = "Category";
+            dimension6.DataMember = "ProductName";
+            dimension6.Name = "Product";
+            this.treeViewDashboardItem1.DataItemRepository.Clear();
+            this.treeViewDashboardItem1.DataItemRepository.Add(dimension5, "DataItem0");
+            this.treeViewDashboardItem1.DataItemRepository.Add(dimension6, "DataItem1");
+            this.treeViewDashboardItem1.DataMember = "SalesPerson";
+            this.treeViewDashboardItem1.DataSource = this.dashboardSqlDataSource1;
+            this.treeViewDashboardItem1.FilterDimensions.AddRange(new DevExpress.DashboardCommon.Dimension[] {
+            dimension5,
+            dimension6});
+            this.treeViewDashboardItem1.InteractivityOptions.IgnoreMasterFilters = true;
+            this.treeViewDashboardItem1.Name = "Category";
+            this.treeViewDashboardItem1.ShowCaption = true;
+            // 
+            // Dashboard1
+            // 
+            this.DataSources.AddRange(new DevExpress.DashboardCommon.IDashboardDataSource[] {
+            this.dashboardSqlDataSource1});
+            this.Items.AddRange(new DevExpress.DashboardCommon.DashboardItem[] {
+            this.gridDashboardItem1,
+            this.treeViewDashboardItem1,
+            this.chartDashboardItem1,
+            this.rangeFilterDashboardItem1});
+            dashboardLayoutItem1.DashboardItem = this.gridDashboardItem1;
+            dashboardLayoutItem1.Weight = 50D;
+            dashboardLayoutItem2.DashboardItem = this.treeViewDashboardItem1;
+            dashboardLayoutItem2.Weight = 50D;
+            dashboardLayoutGroup3.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
+            dashboardLayoutItem1,
+            dashboardLayoutItem2});
+            dashboardLayoutGroup3.DashboardItem = null;
+            dashboardLayoutGroup3.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical;
+            dashboardLayoutGroup3.Weight = 28.895184135977338D;
+            dashboardLayoutItem3.DashboardItem = this.chartDashboardItem1;
+            dashboardLayoutItem3.Weight = 71.104815864022669D;
+            dashboardLayoutGroup2.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
+            dashboardLayoutGroup3,
+            dashboardLayoutItem3});
+            dashboardLayoutGroup2.DashboardItem = null;
+            dashboardLayoutGroup2.Weight = 79.935275080906152D;
+            dashboardLayoutItem4.DashboardItem = this.rangeFilterDashboardItem1;
+            dashboardLayoutItem4.Weight = 20.064724919093852D;
+            dashboardLayoutGroup1.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
+            dashboardLayoutGroup2,
+            dashboardLayoutItem4});
+            dashboardLayoutGroup1.DashboardItem = null;
+            dashboardLayoutGroup1.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical;
+            this.LayoutRoot = dashboardLayoutGroup1;
+            this.Title.Text = "Dashboard";
+            this.ConfigureDataConnection += new DevExpress.DashboardCommon.DashboardConfigureDataConnectionEventHandler(this.Dashboard1_ConfigureDataConnection);
+            ((System.ComponentModel.ISupportInitialize)(dimension1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardSqlDataSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(measure3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rangeFilterDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(dimension6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.treeViewDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardCommon.GridDashboardItem gridDashboardItem1;
+        private DevExpress.DashboardCommon.DashboardSqlDataSource dashboardSqlDataSource1;
+        private DevExpress.DashboardCommon.ChartDashboardItem chartDashboardItem1;
+        private DevExpress.DashboardCommon.RangeFilterDashboardItem rangeFilterDashboardItem1;
+        private DevExpress.DashboardCommon.TreeViewDashboardItem treeViewDashboardItem1;
+    }
+}
---
+++ CS/Dashboard_MFDefaultValues/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using DevExpress.DashboardCommon;
+
+namespace Dashboard_MFDefaultValues {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void dashboardViewer1_MasterFilterDefaultValues(object sender, MasterFilterDefaultValuesEventArgs e) {
+            if (e.ItemComponentName == "gridDashboardItem1") {
+                e.FilterValues = e.AvailableFilterValues.Where(v => (string)v["Sales Person"] == "Laura Callahan");
+            }
+            if (e.ItemComponentName == "treeViewDashboardItem1") {
+                e.FilterValues = e.AvailableFilterValues.Where(v => (string)v["Category"] == "Beverages" || 
+                    (string)v["Product"] == "Aniseed Syrup");
+            }
+        }
+
+        private void dashboardViewer1_RangeFilterDefaultValue(object sender, RangeFilterDefaultValueEventArgs e) {
+            if (e.DashboardItemName == "rangeFilterDashboardItem1") {
+                e.Range = new RangeFilterSelection(new DateTime(2015, 06, 01), new DateTime(2016, 04, 01));
+            }
+        }
+    }
+}
