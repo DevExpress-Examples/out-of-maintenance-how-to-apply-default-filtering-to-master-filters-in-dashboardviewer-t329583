c2871b9f9c9d622995db609b4a3747acd6a4a9f6
DevExpressExampleBot <fakeEmail>
2021-12-08 13:53:03 +0300

    Source auto update [skip ci]

--- VB/Dashboard_MFDefaultValues/Dashboard1.Designer.vb
+++ VB/Dashboard_MFDefaultValues/Dashboard1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace Dashboard_MFDefaultValues
-    Partial Public Class Dashboard1
+Namespace Dashboard_MFDefaultValues
+
+    Partial Class Dashboard1
+
         ''' <summary> 
         ''' Required designer variable.
         ''' </summary>
@@ -10,60 +12,60 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Component Designer generated code"
-
+#Region "Component Designer generated code"
         ''' <summary> 
         ''' Required method for Designer support - do not modify 
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim dimension1 As New DevExpress.DashboardCommon.Dimension()
-            Dim gridDimensionColumn1 As New DevExpress.DashboardCommon.GridDimensionColumn()
-            Dim measure1 As New DevExpress.DashboardCommon.Measure()
-            Dim gridMeasureColumn1 As New DevExpress.DashboardCommon.GridMeasureColumn()
-            Dim dimension2 As New DevExpress.DashboardCommon.Dimension()
-            Dim dimension3 As New DevExpress.DashboardCommon.Dimension()
-            Dim dimension4 As New DevExpress.DashboardCommon.Dimension()
-            Dim dimension5 As New DevExpress.DashboardCommon.Dimension()
-            Dim measure2 As New DevExpress.DashboardCommon.Measure()
-            Dim chartPane1 As New DevExpress.DashboardCommon.ChartPane()
-            Dim simpleSeries1 As New DevExpress.DashboardCommon.SimpleSeries()
-            Dim dimension6 As New DevExpress.DashboardCommon.Dimension()
-            Dim measure3 As New DevExpress.DashboardCommon.Measure()
-            Dim simpleSeries2 As New DevExpress.DashboardCommon.SimpleSeries()
-            Dim dashboardLayoutGroup1 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
-            Dim dashboardLayoutGroup2 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
-            Dim dashboardLayoutGroup3 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
-            Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
-            Dim dashboardLayoutItem2 As New DevExpress.DashboardCommon.DashboardLayoutItem()
-            Dim dashboardLayoutItem3 As New DevExpress.DashboardCommon.DashboardLayoutItem()
-            Dim dashboardLayoutItem4 As New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Dim dimension1 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim gridDimensionColumn1 As DevExpress.DashboardCommon.GridDimensionColumn = New DevExpress.DashboardCommon.GridDimensionColumn()
+            Dim measure1 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim gridMeasureColumn1 As DevExpress.DashboardCommon.GridMeasureColumn = New DevExpress.DashboardCommon.GridMeasureColumn()
+            Dim dimension2 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim dimension3 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim dimension4 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim dimension5 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim measure2 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim chartPane1 As DevExpress.DashboardCommon.ChartPane = New DevExpress.DashboardCommon.ChartPane()
+            Dim simpleSeries1 As DevExpress.DashboardCommon.SimpleSeries = New DevExpress.DashboardCommon.SimpleSeries()
+            Dim dimension6 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim measure3 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim simpleSeries2 As DevExpress.DashboardCommon.SimpleSeries = New DevExpress.DashboardCommon.SimpleSeries()
+            Dim dashboardLayoutGroup1 As DevExpress.DashboardCommon.DashboardLayoutGroup = New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutGroup2 As DevExpress.DashboardCommon.DashboardLayoutGroup = New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutGroup3 As DevExpress.DashboardCommon.DashboardLayoutGroup = New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutItem1 As DevExpress.DashboardCommon.DashboardLayoutItem = New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Dim dashboardLayoutItem2 As DevExpress.DashboardCommon.DashboardLayoutItem = New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Dim dashboardLayoutItem3 As DevExpress.DashboardCommon.DashboardLayoutItem = New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Dim dashboardLayoutItem4 As DevExpress.DashboardCommon.DashboardLayoutItem = New DevExpress.DashboardCommon.DashboardLayoutItem()
             Me.gridDashboardItem1 = New DevExpress.DashboardCommon.GridDashboardItem()
             Me.treeViewDashboardItem1 = New DevExpress.DashboardCommon.TreeViewDashboardItem()
             Me.chartDashboardItem1 = New DevExpress.DashboardCommon.ChartDashboardItem()
             Me.rangeFilterDashboardItem1 = New DevExpress.DashboardCommon.RangeFilterDashboardItem()
             Me.dashboardExtractDataSource1 = New DevExpress.DashboardCommon.DashboardExtractDataSource()
-            CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.treeViewDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension2, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension3, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.chartDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension4, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension5, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure2, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.rangeFilterDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(dimension6, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(measure3, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.dashboardExtractDataSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridDashboardItem1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.treeViewDashboardItem1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension3), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartDashboardItem1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension4), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension5), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.rangeFilterDashboardItem1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension6), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure3), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dashboardExtractDataSource1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
             ' 
             ' gridDashboardItem1
             ' 
@@ -73,7 +75,7 @@
             measure1.DataMember = "Extended Price"
             gridMeasureColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight
             gridMeasureColumn1.AddDataItem("Measure", measure1)
-            Me.gridDashboardItem1.Columns.AddRange(New DevExpress.DashboardCommon.GridColumnBase() { gridDimensionColumn1, gridMeasureColumn1})
+            Me.gridDashboardItem1.Columns.AddRange(New DevExpress.DashboardCommon.GridColumnBase() {gridDimensionColumn1, gridMeasureColumn1})
             Me.gridDashboardItem1.ComponentName = "gridDashboardItem1"
             Me.gridDashboardItem1.DataItemRepository.Clear()
             Me.gridDashboardItem1.DataItemRepository.Add(dimension1, "DataItem0")
@@ -82,7 +84,7 @@
             Me.gridDashboardItem1.DataSource = Me.dashboardExtractDataSource1
             Me.gridDashboardItem1.GridOptions.ColumnWidthMode = DevExpress.DashboardCommon.GridColumnWidthMode.Manual
             Me.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = True
-            Me.gridDashboardItem1.InteractivityOptions.MasterFilterMode = DevExpress.DashboardCommon.DashboardItemMasterFilterMode.Single
+            Me.gridDashboardItem1.InteractivityOptions.MasterFilterMode = DevExpress.DashboardCommon.DashboardItemMasterFilterMode.[Single]
             Me.gridDashboardItem1.Name = "Grid 1"
             Me.gridDashboardItem1.ShowCaption = False
             ' 
@@ -98,7 +100,7 @@
             Me.treeViewDashboardItem1.DataItemRepository.Add(dimension3, "DataItem1")
             Me.treeViewDashboardItem1.DataMember = ""
             Me.treeViewDashboardItem1.DataSource = Me.dashboardExtractDataSource1
-            Me.treeViewDashboardItem1.FilterDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension2, dimension3})
+            Me.treeViewDashboardItem1.FilterDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() {dimension2, dimension3})
             Me.treeViewDashboardItem1.InteractivityOptions.IgnoreMasterFilters = True
             Me.treeViewDashboardItem1.Name = "Category"
             Me.treeViewDashboardItem1.ShowCaption = True
@@ -106,7 +108,7 @@
             ' chartDashboardItem1
             ' 
             dimension4.DataMember = "CategoryName"
-            Me.chartDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension4})
+            Me.chartDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() {dimension4})
             Me.chartDashboardItem1.AxisX.TitleVisible = False
             Me.chartDashboardItem1.ComponentName = "chartDashboardItem1"
             dimension5.DataMember = "OrderDate"
@@ -127,9 +129,9 @@
             chartPane1.SecondaryAxisY.ShowGridLines = False
             chartPane1.SecondaryAxisY.TitleVisible = True
             simpleSeries1.AddDataItem("Value", measure2)
-            chartPane1.Series.AddRange(New DevExpress.DashboardCommon.ChartSeries() { simpleSeries1})
-            Me.chartDashboardItem1.Panes.AddRange(New DevExpress.DashboardCommon.ChartPane() { chartPane1})
-            Me.chartDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension5})
+            chartPane1.Series.AddRange(New DevExpress.DashboardCommon.ChartSeries() {simpleSeries1})
+            Me.chartDashboardItem1.Panes.AddRange(New DevExpress.DashboardCommon.ChartPane() {chartPane1})
+            Me.chartDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() {dimension5})
             Me.chartDashboardItem1.ShowCaption = True
             ' 
             ' rangeFilterDashboardItem1
@@ -148,7 +150,7 @@
             Me.rangeFilterDashboardItem1.Name = "Range Filter 1"
             simpleSeries2.SeriesType = DevExpress.DashboardCommon.SimpleSeriesType.Area
             simpleSeries2.AddDataItem("Value", measure3)
-            Me.rangeFilterDashboardItem1.Series.AddRange(New DevExpress.DashboardCommon.SimpleSeries() { simpleSeries2})
+            Me.rangeFilterDashboardItem1.Series.AddRange(New DevExpress.DashboardCommon.SimpleSeries() {simpleSeries2})
             Me.rangeFilterDashboardItem1.ShowCaption = False
             ' 
             ' dashboardExtractDataSource1
@@ -159,52 +161,55 @@
             ' 
             ' Dashboard1
             ' 
-            Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() { Me.dashboardExtractDataSource1})
-            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() { Me.gridDashboardItem1, Me.treeViewDashboardItem1, Me.chartDashboardItem1, Me.rangeFilterDashboardItem1})
+            Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() {Me.dashboardExtractDataSource1})
+            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() {Me.gridDashboardItem1, Me.treeViewDashboardItem1, Me.chartDashboardItem1, Me.rangeFilterDashboardItem1})
             dashboardLayoutItem1.DashboardItem = Me.gridDashboardItem1
             dashboardLayoutItem1.Weight = 50R
             dashboardLayoutItem2.DashboardItem = Me.treeViewDashboardItem1
             dashboardLayoutItem2.Weight = 50R
-            dashboardLayoutGroup3.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem1, dashboardLayoutItem2})
+            dashboardLayoutGroup3.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() {dashboardLayoutItem1, dashboardLayoutItem2})
             dashboardLayoutGroup3.DashboardItem = Nothing
             dashboardLayoutGroup3.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical
             dashboardLayoutGroup3.Weight = 28.895184135977338R
             dashboardLayoutItem3.DashboardItem = Me.chartDashboardItem1
             dashboardLayoutItem3.Weight = 71.104815864022669R
-            dashboardLayoutGroup2.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutGroup3, dashboardLayoutItem3})
+            dashboardLayoutGroup2.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() {dashboardLayoutGroup3, dashboardLayoutItem3})
             dashboardLayoutGroup2.DashboardItem = Nothing
             dashboardLayoutGroup2.Weight = 79.935275080906152R
             dashboardLayoutItem4.DashboardItem = Me.rangeFilterDashboardItem1
             dashboardLayoutItem4.Weight = 20.064724919093852R
-            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutGroup2, dashboardLayoutItem4})
+            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() {dashboardLayoutGroup2, dashboardLayoutItem4})
             dashboardLayoutGroup1.DashboardItem = Nothing
             dashboardLayoutGroup1.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical
             Me.LayoutRoot = dashboardLayoutGroup1
             Me.Title.Text = "Dashboard"
-            CType(dimension1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension2, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension3, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.treeViewDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension4, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension5, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure2, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.chartDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(dimension6, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(measure3, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.rangeFilterDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.dashboardExtractDataSource1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
-
+            AddHandler Me.ConfigureDataConnection, New DevExpress.DashboardCommon.DashboardConfigureDataConnectionEventHandler(AddressOf Me.Dashboard1_ConfigureDataConnection)
+            CType((dimension1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridDashboardItem1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension3), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.treeViewDashboardItem1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension4), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension5), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartDashboardItem1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension6), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure3), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.rangeFilterDashboardItem1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dashboardExtractDataSource1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
         End Sub
 
-        #End Region
-
+#End Region
         Private gridDashboardItem1 As DevExpress.DashboardCommon.GridDashboardItem
+
         Private chartDashboardItem1 As DevExpress.DashboardCommon.ChartDashboardItem
+
         Private rangeFilterDashboardItem1 As DevExpress.DashboardCommon.RangeFilterDashboardItem
+
         Private treeViewDashboardItem1 As DevExpress.DashboardCommon.TreeViewDashboardItem
+
         Private dashboardExtractDataSource1 As DevExpress.DashboardCommon.DashboardExtractDataSource
     End Class
 End Namespace
--- VB/Dashboard_MFDefaultValues/Form1.vb
+++ VB/Dashboard_MFDefaultValues/Form1.vb
@@ -1,9 +1,10 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports System
 Imports System.Linq
 
 Namespace Dashboard_MFDefaultValues
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraEditors.XtraForm
 
         Public Sub New()
@@ -12,7 +13,6 @@ Namespace Dashboard_MFDefaultValues
             AddHandler dashboardViewer1.ConfigureDataConnection, AddressOf DashboardViewer1_ConfigureDataConnection
             AddHandler dashboardViewer1.MasterFilterDefaultValues, AddressOf DashboardViewer1_MasterFilterDefaultValues
             AddHandler dashboardViewer1.RangeFilterDefaultValue, AddressOf DashboardViewer1_RangeFilterDefaultValue
-
             dashboardViewer1.Dashboard = New Dashboard1()
         End Sub
 
@@ -22,17 +22,18 @@ Namespace Dashboard_MFDefaultValues
         End Sub
 
         Private Sub DashboardViewer1_MasterFilterDefaultValues(ByVal sender As Object, ByVal e As MasterFilterDefaultValuesEventArgs)
-            If e.ItemComponentName = "gridDashboardItem1" Then
-                e.FilterValues = e.AvailableFilterValues.Where(Function(v) CStr(v("Sales Person")) = "Laura Callahan")
+            If Equals(e.ItemComponentName, "gridDashboardItem1") Then
+                e.FilterValues = e.AvailableFilterValues.Where(Function(v) Equals(CStr(v("Sales Person")), "Laura Callahan"))
             End If
-            If e.ItemComponentName = "treeViewDashboardItem1" Then
-                e.FilterValues = e.AvailableFilterValues.Where(Function(v) CStr(v("Category")) = "Beverages" OrElse CStr(v("Product")) = "Aniseed Syrup")
+
+            If Equals(e.ItemComponentName, "treeViewDashboardItem1") Then
+                e.FilterValues = e.AvailableFilterValues.Where(Function(v) Equals(CStr(v("Category")), "Beverages") OrElse Equals(CStr(v("Product")), "Aniseed Syrup"))
             End If
         End Sub
 
         Private Sub DashboardViewer1_RangeFilterDefaultValue(ByVal sender As Object, ByVal e As RangeFilterDefaultValueEventArgs)
-            If e.DashboardItemName = "rangeFilterDashboardItem1" Then
-                e.Range = New RangeFilterSelection(New Date(2015, 06, 01), New Date(2016, 04, 01))
+            If Equals(e.DashboardItemName, "rangeFilterDashboardItem1") Then
+                e.Range = New RangeFilterSelection(New DateTime(2015, 06, 01), New DateTime(2016, 04, 01))
             End If
         End Sub
     End Class
